97586afbf510a399bb81373937383074cfa358f6
fiban123 <fabianblatter09@gmail.com>
2026-05-11 10:20:46 +0200

    add white monster tracker

---
+++ data.js
@@ -0,0 +1,29 @@
+const monsterSites = [
+    {
+        name: "Brack",
+        url: "https://www.brack.ch/monster-energy-drink-ultra-12-x-0-5-l-1571266",
+        status: "out_of_stock",
+        lastChecked: "11-05-26",
+        totalPrice: 20.40,
+        count: 12,
+        discountPercent: 30
+    },
+    {
+        name: "Galaxus",
+        url: "https://www.galaxus.ch/de/s7/product/monster-energy-ultra-12-x-50-cl-softdrink-33402615",
+        status: "out_of_stock",
+        lastChecked: "11-05-26",
+        totalPrice: 20.70,
+        count: 12,
+        discountPercent: 0
+    },
+    {
+        name: "Ottos",
+        url: "https://www.ottos.ch/de/supermarkt-weine/getraenke/energydrinks/monster-ultra-white-12-x-500-ml/p/405756",
+        status: "out_of_stock",
+        lastChecked: "11-05-26",
+        totalPrice: 21.00,
+        count: 12,
+        discountPercent: 43
+    }
+];
