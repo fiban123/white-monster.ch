26746a1638e7dda78f4a32069b391add8ee38f2e
fiban123 <144467048+fiban123@users.noreply.github.com>
2026-05-18 08:19:15 +0200

    update values

--- data.js
+++ data.js
@@ -3,7 +3,7 @@ const monsterSites = [
         name: "Brack",
         url: "https://www.brack.ch/monster-energy-drink-ultra-12-x-0-5-l-1571266",
         status: "out_of_stock",
-        lastChecked: "11-05-26",
+        lastChecked: "18-05-26",
         totalPrice: 20.40,
         count: 12,
         discountPercent: 30
@@ -11,8 +11,8 @@ const monsterSites = [
     {
         name: "Galaxus",
         url: "https://www.galaxus.ch/de/s7/product/monster-energy-ultra-12-x-50-cl-softdrink-33402615",
-        status: "out_of_stock",
-        lastChecked: "11-05-26",
+        status: "available",
+        lastChecked: "18-05-26",
         totalPrice: 20.70,
         count: 12,
         discountPercent: 0
@@ -21,7 +21,7 @@ const monsterSites = [
         name: "Ottos",
         url: "https://www.ottos.ch/de/supermarkt-weine/getraenke/energydrinks/monster-ultra-white-12-x-500-ml/p/405756",
         status: "out_of_stock",
-        lastChecked: "11-05-26",
+        lastChecked: "18-05-26",
         totalPrice: 21.00,
         count: 12,
         discountPercent: 43
@@ -30,16 +30,16 @@ const monsterSites = [
         name: "Coop",
         url: "https://www.coop.ch/en/food/drinks/soft-drinks/sport-energy-drinks/energy-drinks/monster-ultra-white-4x50cl/p/7211277#tab=product-information",
         status: "out_of_stock",
-        lastChecked: "11-05-26",
+        lastChecked: "18-05-26",
         totalPrice: 6.80,
         count: 4,
-        discountPercent: 0
+        discountPercent: 25
     },
     {
         name: "Migros",
         url: "https://www.migros.ch/de/product/mo/10650245",
         status: "out_of_stock",
-        lastChecked: "11-05-26",
+        lastChecked: "18-05-26",
         totalPrice: 10.20,
         count: 6,
         discountPercent: 30
