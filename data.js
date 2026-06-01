65857c977f383a4bca6eff9f876a0cbd8b29f21d
fiban123 <144467048+fiban123@users.noreply.github.com>
2026-06-01 11:04:07 +0200

    update data

--- data.js
+++ data.js
@@ -2,16 +2,16 @@ const monsterSites = [
     {
         name: "Brack",
         url: "https://www.brack.ch/monster-energy-drink-ultra-12-x-0-5-l-1571266",
-        status: "out_of_stock",
+        status: "available",
         lastChecked: "21-05-26",
         totalPrice: 20.40,
         count: 12,
-        discountPercent: 30
+        discountPercent: 0
     },
     {
         name: "Galaxus",
         url: "https://www.galaxus.ch/de/s7/product/monster-energy-ultra-12-x-50-cl-softdrink-33402615",
-        status: "out_of_stock",
+        status: "available",
         lastChecked: "21-05-26",
         totalPrice: 20.70,
         count: 12,
@@ -38,7 +38,7 @@ const monsterSites = [
     {
         name: "Migros",
         url: "https://www.migros.ch/de/product/mo/10650245",
-        status: "out_of_stock",
+        status: "available",
         lastChecked: "21-05-26",
         totalPrice: 10.20,
         count: 6,
